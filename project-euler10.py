e6eba40dc30b01caab3eaa6df8f3d8a329958453
CopperSulph8 <102553076+CopperSulph8@users.noreply.github.com>
2022-03-29 20:34:19 +0100

    Add files via upload

---
+++ project-euler10.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+
+"""
+
+import math
+
+def isPrime(n): # Using primality criteria to avoid factorisation 
+    if n == 1:
+        return False
+    elif n < 4:
+        return True
+    elif n % 2 == 0:
+        return False
+    elif n < 9:
+        return True
+    elif n % 3 == 0:
+        return False
+    else:
+        r = math.floor(math.sqrt(n))
+        f = 5
+        while f <= r:
+            if n % f == 0:
+                return False
+            if n % (f+2) == 0:
+                return False
+            f += 6
+        return True
+  
+    
+sumPrimes = 0
+n = 2
+while n <= 2000000:
+    if isPrime(n):
+        sumPrimes += n
+    n += 1
+    
+print(sumPrimes)
